4c4e569982a7a1694f49a31b27b58791dda42a65
thomas <tom.angotti11@gmail.com>
2022-12-13 11:31:40 -0700

    commit tree 194ad788def9c2ea4adb870efbe994b1b4b4a629

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    return cats.splice(2);
+};
+
+function destructivelyRemoveFirstCat() {
+    return cats.splice(0, 1);
+};
+
+function appendCat(name){
+    let newCats ='';
+    return newCats = [...cats, name];
+};
+
+function prependCat(name){
+    let newCats ='';
+    return newCats = [name, ...cats];
+};
+
+function removeLastCat() {
+    let newCats = '';
+    return newCats = cats.slice(0, 2);
+}
+
+function removeFirstCat() {
+    let newCats = '';
+    return newCats = cats.slice(-2);
+}
